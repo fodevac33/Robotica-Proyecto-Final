502eaf8d489ea048ab1e17d1d7e6bf63ab428a44
fodevac <fodevac@gmail.com>
2025-07-16 21:54:30 -0500

    Better build script

--- build_robot.sh
+++ build_robot.sh
@@ -8,8 +8,10 @@ fi
 
 WORKSPACE=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
 
-echo "[1/2] Build workspace…"
+echo "[1/3] Cleaning up old build…"
+rm -rf $WORKSPACE/build/ $WORKSPACE/log/ $WORKSPACE/install/
+echo "[2/3] Build workspace…"
 colcon build --symlink-install
-echo "[2/2] Source overlay…"
+echo "[3/3] Source overlay…"
 source "$WORKSPACE/install/setup.bash"
 echo "✔️  Overlay listo en esta terminal."
